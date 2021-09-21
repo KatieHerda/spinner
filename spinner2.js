68561502772297b4cc8b24d968f9cc82ea55b24a
Katie <katie.herda2@gmail.com>
2021-09-21 20:54:21 +0000

    refactored spinner1.js by using a loop to delay the printing of each character to simulate an animation.

---
+++ spinner2.js
@@ -0,0 +1,14 @@
+const spinner = "\r| \r/ \r- \r\\ \r| \r/ \r- \r\\\n";
+
+let delay = 0;
+for (const char of spinner) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay += 100);
+}
+
+
+
+// setTimeout(() => {
+//   process.stdout.write(char);
+// }, delay += 700);
